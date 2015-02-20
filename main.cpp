f3c260c76eefc1c725b79c78ad2b439b1ae8ccdc
Joseph <jyoon@nd.edu>
2015-02-20 13:06:38 -0500

    commented directions for main

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
-/* Joseph Yoon*/
+/* Joseph Yoon, Lab 5, CSE20212
+	Part 2: Sudoku driver
+	reads in provided sudoku/wordoku using a puzzle constructor
+	stores it in a vector of vectors (2D array) - private data member of the Puzzle class
+	displays unfinished puzzles to the screen
+*/
+
+int main(){
+
+	return 0;
+}
+
