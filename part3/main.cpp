014d9b106a9a151d5796403466de8eb01f1109a4
Joseph <jyoon@nd.edu>
2015-02-26 11:16:10 -0500

    final without coder challenge

--- part3/main.cpp
+++ part3/main.cpp
@@ -1,5 +1,5 @@
 /* Joseph Yoon, Lab 5, CSE20212
-	Part 2: Sudoku driver
+	Part 3: Sudoku driver
 	reads in provided sudoku/wordoku using a puzzle constructor
 	stores it in a vector of vectors (2D array) - private data member of the Puzzle class
 	displays unfinished puzzles to the screen
@@ -19,17 +19,17 @@ int main(){
 	Puzzle<int> sudoku(filename); //instantiate a Puzzle object of type int
 	
 	do{
-		sudoku.print();
-		int a, b, c;
-		cout << "Row: ";
+		sudoku.print(); //print out the board
+		int a, b, c; 
+		cout << "Row: "; // ask for row column and number from user
 		cin >> a;
 		cout << "Column: ";
 		cin >> b;
 		cout << "Number: ";
 		cin >> c;
-		sudoku.play(a, b, c);
-	}while(!sudoku.winner());	
-	if(sudoku.winner())
+		sudoku.play(a, b, c); //use play function to check if numbers are valid and can be put into the board
+	}while(!sudoku.winner()); // continue while the board is not completed	
+	if(sudoku.winner()) //if the board is full at the end then print out winning output, doesn't have to be in if statement
 		cout << "Congratulations! You have won!" << endl;
 	return 0;
 }
--- part3/puzzle.h
+++ part3/puzzle.h
@@ -1,5 +1,5 @@
 /*Joseph Yoon, Lab 5, CSE20212
-	Part 2
+	Part 3
 	puzzle class header file
 	templated to easily create Puzzle<int> or Puzzle<char>
 */
@@ -31,64 +31,64 @@ class Puzzle {
 
 //beginning of implementation
 template <typename T>
-int Puzzle<T>::winner(){
+int Puzzle<T>::winner(){ //checks if board is full
 	for(int x = 0; x < board.size()-1; x++)
 		for(int y = 0; y < board.size()-1; y++)
-			if(board[x][y] == 0)
+			if(board[x][y] == 0) //if there is a 0, still empty
 				return 0;
 	return 1;
 }
 
 template <typename T>
 void Puzzle<T>::play(int r, int c, int n){
-	int check = 0;
-	if(!(r > 9 || r < 1 || c > 9 || c < 1 || n > 9 || n < 1)){
-		if(checkRow(r, n)){
+	int check = 0; //counter to see how many problems there are
+	if(!(r > 9 || r < 1 || c > 9 || c < 1 || n > 9 || n < 1)){ //make sure within bounds
+		if(checkRow(r, n)){ //checks row
 			check++;
 			cout << "Duplicate number in row." << endl;
 		}
-		if(checkColumn(c, n)){
+		if(checkColumn(c, n)){ //checks column
 			check++;
 			cout << "Duplicate number in column." << endl;
 		}
-		if(checkGrid(r, c, n)){
+		if(checkGrid(r, c, n)){ //checks grid
 			check++;
 			cout << "Duplicate number in grid." << endl;
 		}
-		if(board[r-1][c-1] != 0){
+		if(board[r-1][c-1] != 0){ //makes sure space is empty
 			check++;
 			cout << "This space is not empty." << endl;
 		}
-		if(check == 0)
+		if(check == 0) //if no errors, place number
 			board[r-1][c-1] = n;
 	}
-	else
+	else //invalid numbers, output error message
 		cout << "Please enter valid numbers." << endl << "Numbers must be between 1-9 (inclusive)." << endl;
 
 }
 
 template <typename T>
-int Puzzle<T>::checkRow(int row, int num){
+int Puzzle<T>::checkRow(int row, int num){ //function to check rows
 	for(int c = 0; c < board.size()-1; c++)
-		if(board[row-1][c] == num)
+		if(board[row-1][c] == num) //go through whole row
 			return 1;	
 	return 0;
 }
 
 template <typename T>
-int Puzzle<T>::checkColumn(int col, int num){
-	for(int r = 0; r < board.size()-1; r++)
-		if(board[r][col-1] == num)
+int Puzzle<T>::checkColumn(int col, int num){ //function to check columns
+	for(int r = 0; r < board.size()-1; r++) 
+		if(board[r][col-1] == num) // go through whole column
 			return 1;
 	return 0;
 }
 
 template <typename T>
-int Puzzle<T>::checkGrid(int row, int col, int num){
-	int rowStart = (row-1) - (row-1)%3;
-	int colStart = (col-1) - (col-1)%3;
-	for(int r = rowStart; r < (rowStart+3); r++){
-		for(int c = colStart; c < (colStart+3); c++){
+int Puzzle<T>::checkGrid(int row, int col, int num){ //function to check grid
+	int rowStart = (row-1) - (row-1)%3; //easy computation for row variable
+	int colStart = (col-1) - (col-1)%3; //easy computation for col variable
+	for(int r = rowStart; r < (rowStart+3); r++){ //two for loops to go through grid
+		for(int c = colStart; c < (colStart+3); c++){ 
 			if(board[r][c] == num)
 				return 1;
 		}
